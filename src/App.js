7c6006e7b8652449a3868a8369fb691300a16a24
Your Name <you@example.com>
2024-10-17 07:47:32 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -7,14 +7,6 @@ import LoginForm from "./pages/LoginPage";
 import RegisterForm from "./pages/Register";
 import ProductList from "./pages/product";
 import Cart from "./pages/Cart";
-<<<<<<< HEAD
-import MeasureSize from './pages/MeasureSize'; 
-import WarrantyAndCare from './pages/WarrantyAndCare'; 
-import DeliveryAndReturns from './pages/DeliveryAndReturns'; 
-
-
-=======
->>>>>>> 02e3e5fc85567f1b0acf9d7d88f577a3d244a28c
 
 function App() {
   return (
@@ -23,20 +15,9 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="product" element={<ProductList />} />
-<<<<<<< HEAD
-          <Route path="cart" element={<Cart />} />
-          <Route path="login" element={<LoginForm />} />
-          <Route path="register" element={<RegisterForm />} />
-          <Route path="/measure-size" element={<MeasureSize />} />
-          <Route path="/warranty-and-care" component={<WarrantyAndCare />} />
-          <Route path="/delivery-and-returns" component={<DeliveryAndReturns/>} />
-
-
-=======
           <Route path="cart" element={<ProductList />} />
           <Route path="login" element={<LoginForm />} />
           <Route path="register" element={<RegisterForm />} />
->>>>>>> 02e3e5fc85567f1b0acf9d7d88f577a3d244a28c
         </Route>
       </Routes>
     </Router>
