12488522f5112b4872b554ff915a78ca18caa5be
Tien0404 <skytua121@gmail.com>
2024-11-25 18:52:36 +0700

    update admin

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,21 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controller/userController');
-const authMiddleware = require('../middleware/auth');
+const userController = require("../controller/userController");
+const authMiddleware = require("../middleware/auth");
 
 // User routes
-router.post('/register', userController.register);
-router.post('/verify', userController.verifyEmail);
-router.post('/login', userController.login);
+router.post("/register", userController.register);
+router.post("/verify", userController.verifyEmail);
+router.post("/login", userController.login);
 
-router.get('/' ,userController.getAllUsers)
-router.put('/:id', userController.update);
-router.delete('/:id', userController.delete);
-router.get('/:id', userController.get);
-router.post('/forgot-password', userController.forgotPassword);
-router.post('/reset-password', userController.resetPassword);
+router.get("/", userController.getAllUsers);
+router.put("/:id", userController.update);
+router.delete("/:id", userController.delete);
+router.get("/:id", userController.get);
+router.post("/forgot-password", userController.forgotPassword);
+router.post("/reset-password", userController.resetPassword);
 
 router.use(authMiddleware);
-
 
 module.exports = router;
