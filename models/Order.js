55d906e079fb90f1ac26edaa6a2c2fa1f20f0912
Your Name <you@example.com>
2024-12-05 13:03:35 +0700

    moi

--- models/Order.js
+++ models/Order.js
@@ -6,7 +6,11 @@ const orderSchema = new mongoose.Schema({
         ref: 'User',
         required: true
     },
-
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
     paymentId: {
         type: String,
         required: true
